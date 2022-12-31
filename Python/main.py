517f33b7d40fa9069b7b299a192245c3f349be6c
Ramesh N <111989928+Raam043@users.noreply.github.com>
2022-12-31 19:32:36 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,7 @@
-r = Ramesh NB
-print ("r")
-
+import string
+print("Alphabet from a-z:")
+for letter in string.ascii_lowercase:
+   print(letter, end =" ")
+print("\nAlphabet from A-Z:")
+for letter in string.ascii_uppercase:
+   print(letter, end =" ")
