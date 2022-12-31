36bbcb00ae406ca3cc76a56dcbe043ca02142b0a
Ramesh N <111989928+Raam043@users.noreply.github.com>
2022-12-31 14:25:11 +0530

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,15 +1,3 @@
-# Program to display calendar of the given month and year
-
-# importing calendar module
-import calendar
-
-yy = 2022  # year
-mm = 12    # month
-
-# To take month and year input from the user
-# yy = int(input("Enter year: "))
-# mm = int(input("Enter month: "))
-
-# display the calendar
-print(calendar.month(yy, mm))
+r = Ramesh NB
+print ("r")
 
